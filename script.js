a2242ea9d77c773d4c305c0303e88498c60a9e2a
Va1bhavS4hu <vaibhavsahu50cs@gmail.com>
2025-09-26 21:35:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Just a small interaction example
+document.getElementById("contactBtn").addEventListener("click", function() {
+  alert("Thanks for visiting! You can contact me via Email or Instagram.");
+});
+
